1f5750dcb1d03cf364765c981d48d7b7d35e479a
atsalrizandri <atsalrizandrii@gmail.com>
2024-09-30 00:28:51 +0700

    Submit task of week modul TP 1

---
+++ 01_Pengenalan_CPP_Bagian_1/TP/SOAL_01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string nama, nim;
+    cout << "Siapa nama anda? ";
+    cin >> nama;
+    cout << "Berapa nim anda? ";
+    cin >> nim;
+    cout << "Nama saya: " << nama << endl;
+    // Muhammmad Atsal Rizandri
+    cout << "NIM saya: " << nim << endl;
+    // 21104057
+    return 0;
+}
---
+++ 01_Pengenalan_CPP_Bagian_1/TP/SOAL_03.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int bil1 = 2, bil2 = 3, hasil;
+    // Muhammad Atsal Rizandri | 21104057
+    hasil = bil1 > bil2;
+    cout << hasil << endl;
+
+    hasil = bil1 >= bil2;
+    cout << hasil << endl;
+
+    hasil = bil1 < bil2;
+    cout << hasil << endl;
+
+    hasil = bil1 <= bil2;
+    cout << hasil << endl;
+
+    hasil = bil1 == bil2;
+    cout << hasil << endl;
+
+    hasil = bil1 != bil2;
+    cout << hasil << endl;
+    return 0;
+}
